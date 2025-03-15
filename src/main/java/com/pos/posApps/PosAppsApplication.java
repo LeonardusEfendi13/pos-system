fb2755c79768ebe35e6df3ddf0b3a40f1fad2103
Leo <leonardusefendi1312@gmail.com>
2025-03-15 22:11:19 +0700

    init push

---
+++ src/main/java/com/pos/posApps/PosAppsApplication.java
@@ -0,0 +1,13 @@
+package com.pos.posApps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosAppsApplication.class, args);
+	}
+
+}
