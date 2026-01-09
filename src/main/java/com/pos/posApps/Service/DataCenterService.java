6f23ee2d994ba25d4c6f7e6d44129b289448b8b3
Leonardus Efendi <leonardusefendi1312@gmail.com>
2026-01-09 17:36:27 +0700

    Fix : change db name from dev into postgres

--- src/main/java/com/pos/posApps/Service/DataCenterService.java
+++ src/main/java/com/pos/posApps/Service/DataCenterService.java
@@ -93,7 +93,7 @@ public class DataCenterService {
                 "--if-exists",
                 "--no-owner",
                 "--no-privileges",
-                "-d", "dev",
+                "-d", "postgres",
                 dumpFile.getAbsolutePath()
         );
 
