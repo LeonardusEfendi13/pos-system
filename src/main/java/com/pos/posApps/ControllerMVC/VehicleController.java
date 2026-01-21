be48e351516c56caf757cd747b550b3271a900b3
Leonardus Efendi <leonardusefendi1312@gmail.com>
2026-01-21 23:42:43 +0700

    Feat : New Controller, Service and html (Vehicle)

---
+++ src/main/java/com/pos/posApps/ControllerMVC/VehicleController.java
@@ -0,0 +1,141 @@
+package com.pos.posApps.ControllerMVC;
+
+import com.pos.posApps.DTO.Dtos.SidebarDTO;
+import com.pos.posApps.Entity.AccountEntity;
+import com.pos.posApps.Entity.ClientEntity;
+import com.pos.posApps.Entity.SupplierEntity;
+import com.pos.posApps.Entity.VehicleEntity;
+import com.pos.posApps.Service.AuthService;
+import com.pos.posApps.Service.SidebarService;
+import com.pos.posApps.Service.SupplierService;
+import com.pos.posApps.Service.VehicleService;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+import static com.pos.posApps.Constants.Constant.authSessionKey;
+
+@Controller
+@RequestMapping("vehicle")
+@AllArgsConstructor
+public class VehicleController {
+
+    private AuthService authService;
+    private VehicleService vehicleService;
+    private SidebarService sidebarService;
+
+    @GetMapping
+    public String displayVehicle(HttpSession session, Model model){
+        Long clientId;
+        String token;
+        try{
+            token = (String) session.getAttribute(authSessionKey);
+            clientId = authService.validateToken(token).getClientEntity().getClientId();
+        }catch (Exception e){
+            return "redirect:/login";
+        }
+
+        List<VehicleEntity> vehicleEntityList = vehicleService.getVehicleList();
+        model.addAttribute("vehicleData", vehicleEntityList);
+        model.addAttribute("activePage", "vehicle");
+        SidebarDTO sidebarData = sidebarService.getSidebarData(clientId, token);
+        model.addAttribute("sidebarData", sidebarData);
+        return "display_vehicle";
+    }
+
+//    @PostMapping("/add")
+//    public String addSupplier(String supplierName, HttpSession session, RedirectAttributes redirectAttributes){
+//        AccountEntity accEntity;
+//        ClientEntity clientData;
+//        try{
+//            String token = (String) session.getAttribute(authSessionKey);
+//            accEntity = authService.validateToken(token);
+//            clientData = accEntity.getClientEntity();
+//            if (clientData.getClientId() == null) {
+//                return "redirect:/login";
+//            }
+//        }catch (Exception e){
+//            return "redirect:/login";
+//        }
+//
+//        if (authService.hasAccessToModifyData(accEntity.getRole())) {
+//            boolean isInserted = supplierService.insertSupplier(supplierName, clientData);
+//            if (isInserted) {
+//                redirectAttributes.addFlashAttribute("status", "success");
+//                redirectAttributes.addFlashAttribute("message", "Data created");
+//                return "redirect:/supplier";
+//            }
+//            redirectAttributes.addFlashAttribute("status", "failed");
+//            redirectAttributes.addFlashAttribute("message", "Failed to create data");
+//            return "redirect:/supplier";
+//        }
+//        return "redirect:/login";
+//    }
+//
+//    @PostMapping("/edit")
+//    public String editSupplier(Long supplierId, String supplierName, HttpSession session, RedirectAttributes redirectAttributes){
+//        AccountEntity accEntity;
+//        ClientEntity clientData;
+//        try{
+//            String token = (String) session.getAttribute(authSessionKey);
+//            accEntity = authService.validateToken(token);
+//            clientData = accEntity.getClientEntity();
+//            if (clientData.getClientId() == null) {
+//                return "redirect:/login";
+//            }
+//        }catch (Exception e){
+//            return "redirect:/login";
+//        }
+//
+//        if (authService.hasAccessToModifyData(accEntity.getRole())) {
+//            boolean isEdited = supplierService.editSupplier(supplierId, supplierName, clientData);
+//            if(isEdited){
+//                redirectAttributes.addFlashAttribute("status", "success");
+//                redirectAttributes.addFlashAttribute("message", "Data Edited");
+//                return "redirect:/supplier";
+//            }
+//            redirectAttributes.addFlashAttribute("status", "failed");
+//            redirectAttributes.addFlashAttribute("message", "Failed to edit Data");
+//            return "redirect:/supplier";
+//        }
+//        return "redirect:/login";
+//    }
+//
+//    @PostMapping("/delete/{supplierId}")
+//    public String disableSupplier(
+//            @PathVariable("supplierId") Long supplierId, HttpSession session, RedirectAttributes redirectAttributes){
+//        AccountEntity accEntity;
+//        ClientEntity clientData;
+//        try{
+//            String token = (String) session.getAttribute(authSessionKey);
+//            accEntity = authService.validateToken(token);
+//            clientData = accEntity.getClientEntity();
+//            if (clientData.getClientId() == null) {
+//                return "redirect:/login";
+//            }
+//        }catch (Exception e){
+//            return "redirect:/login";
+//        }
+//
+//        if (authService.hasAccessToModifyData(accEntity.getRole())) {
+//            boolean isEdited = supplierService.disableSupplier(supplierId, clientData);
+//            if(isEdited){
+//                redirectAttributes.addFlashAttribute("status", "success");
+//                redirectAttributes.addFlashAttribute("message", "Data Deleted");
+//                return "redirect:/supplier";
+//            }
+//            redirectAttributes.addFlashAttribute("status", "failed");
+//            redirectAttributes.addFlashAttribute("message", "Failed to delete data");
+//            return "redirect:/supplier";
+//        }
+//        return "redirect:/login";
+//    }
+}
---
+++ src/main/java/com/pos/posApps/Service/VehicleService.java
@@ -0,0 +1,100 @@
+package com.pos.posApps.Service;
+
+import com.pos.posApps.Entity.ClientEntity;
+import com.pos.posApps.Entity.SupplierEntity;
+import com.pos.posApps.Entity.VehicleEntity;
+import com.pos.posApps.Repository.SupplierRepository;
+import com.pos.posApps.Repository.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.pos.posApps.Util.Generator.getCurrentTimestamp;
+
+@Service
+public class VehicleService {
+    @Autowired
+    VehicleRepository vehicleRepository;
+
+//    public String getSupplierDataById(Long supplierId, Long clientId){
+//        Optional<SupplierEntity> supplierOpt =  supplierRepository.findFirstBySupplierIdAndDeletedAtIsNullAndClientEntity_ClientId(supplierId, clientId);
+//        if(supplierOpt.isEmpty()){
+//            return "INVALID";
+//        }
+//        return supplierOpt.get().getSupplierName();
+//    }
+
+    public List<VehicleEntity> getVehicleList(){
+        return vehicleRepository.findAll();
+    }
+
+//    @Transactional
+//    public Boolean insertSupplier(String supplierName, ClientEntity clientData){
+//        try{
+//            SupplierEntity supplierEntity = supplierRepository.findFirstBySupplierNameIgnoreCaseAndClientEntity_ClientIdAndDeletedAtIsNull(supplierName, clientData.getClientId());
+//            if(supplierEntity != null){
+//                return false;
+//            }
+//
+////            SupplierEntity lastSupplierData = supplierRepository.findFirstByOrderBySupplierIdDesc();
+////            Long lastSupplierId = lastSupplierData == null ? 0L : lastSupplierData.getSupplierId();
+////            Long newSupplierId = Generator.generateId(lastSupplierId);
+//            SupplierEntity newSupplierEntity = new SupplierEntity();
+////            newSupplierEntity.setSupplierId(newSupplierId);
+//            newSupplierEntity.setSupplierName(supplierName);
+//            newSupplierEntity.setClientEntity(clientData);
+//            supplierRepository.save(newSupplierEntity);
+//            return true;
+//        }catch (Exception e){
+//            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+//            return false;
+//        }
+//    }
+//
+//    @Transactional
+//    public Boolean editSupplier(Long supplierId, String supplierName, ClientEntity clientData){
+//        try{
+//            SupplierEntity supplierEntity = supplierRepository.findFirstBySupplierIdAndClientEntity_ClientIdAndDeletedAtIsNull(supplierId, clientData.getClientId());
+//            if(supplierEntity == null){
+//                return false;
+//            }
+//
+//            // Check if another supplier with the same name exists (excluding the current one)
+//            boolean nameExists = supplierRepository.existsBySupplierNameIgnoreCaseAndClientEntity_ClientIdAndDeletedAtIsNullAndSupplierIdNot(
+//                    supplierName, clientData.getClientId(), supplierId
+//            );
+//
+//            if (nameExists) {
+//                return false; // Duplicate name exists
+//            }
+//
+//            supplierEntity.setSupplierName(supplierName);
+//            supplierRepository.save(supplierEntity);
+//            return true;
+//        }catch (Exception e){
+//            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+//            return false;
+//        }
+//    }
+//
+//    @Transactional
+//    public Boolean disableSupplier(Long supplierId, ClientEntity clientData){
+//        try{
+//            SupplierEntity supplierEntity = supplierRepository.findFirstBySupplierIdAndClientEntity_ClientIdAndDeletedAtIsNull(supplierId, clientData.getClientId());
+//            if(supplierEntity == null){
+//                return false;
+//            }
+//
+//            supplierEntity.setDeletedAt(getCurrentTimestamp());
+//            supplierRepository.save(supplierEntity);
+//            return true;
+//        }catch (Exception e){
+//            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+//            return false;
+//        }
+//    }
+}
