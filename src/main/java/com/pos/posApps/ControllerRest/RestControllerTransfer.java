46797adbafc0bb03b1a75b0fdd23de70087f4cac
Leonardus Efendi <leonardusefendi1312@gmail.com>
2026-02-07 16:50:19 +0700

    Feat : Versioning, add restController and MVC Controller

---
+++ src/main/java/com/pos/posApps/ControllerRest/RestControllerTransfer.java
@@ -0,0 +1,94 @@
+package com.pos.posApps.ControllerRest;
+
+import com.pos.posApps.DTO.Dtos.CreateTransactionRequest;
+import com.pos.posApps.DTO.Dtos.PenjualanDTO;
+import com.pos.posApps.DTO.Dtos.ResponseInBoolean;
+import com.pos.posApps.Entity.AccountEntity;
+import com.pos.posApps.Entity.ClientEntity;
+import com.pos.posApps.Service.AuthService;
+import com.pos.posApps.Service.KasirService;
+import com.pos.posApps.Service.PenjualanService;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
+
+import static com.pos.posApps.Constants.Constant.authSessionKey;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+@RestController
+@RequestMapping("api/transfer")
+@AllArgsConstructor
+public class RestControllerTransfer {
+    private AuthService authService;
+    private KasirService kasirService;
+    private PenjualanService penjualanService;
+
+    @GetMapping("/transaction/list")
+    public ResponseEntity<List<PenjualanDTO>> getList(HttpSession session){
+        ClientEntity clientData;
+        try {
+            String token = (String) session.getAttribute(authSessionKey);
+            clientData = authService.validateToken(token).getClientEntity();
+            List<PenjualanDTO> data = penjualanService.getLast10Transaction(clientData.getClientId());
+            return ResponseEntity.ok(data);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(UNAUTHORIZED).body(Collections.emptyList());
+        }
+    }
+
+    @GetMapping("/transaction/revenue")
+    public ResponseEntity<BigDecimal> getRevenue(HttpSession session){
+        ClientEntity clientData;
+        try {
+            String token = (String) session.getAttribute(authSessionKey);
+            clientData = authService.validateToken(token).getClientEntity();
+            BigDecimal revenue = penjualanService.getTotalRevenues(clientData.getClientId());
+            return ResponseEntity.ok(revenue);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(UNAUTHORIZED).body(BigDecimal.ZERO);
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addTransaction(@RequestBody CreateTransactionRequest req, HttpSession session){
+        AccountEntity accountData;
+        try {
+            String token = (String) session.getAttribute(authSessionKey);
+            accountData = authService.validateToken(token);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(UNAUTHORIZED).body("Unauthorized access");
+        }
+
+        ResponseInBoolean response = kasirService.createTransaction(req, accountData);
+        if(response.isStatus()){
+            return ResponseEntity.ok(response.getMessage());
+        }
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(response.getMessage());
+    }
+
+    @PostMapping("/edit/{transactionId}")
+    public ResponseEntity<String> editTransaction(@PathVariable("transactionId") Long transactionId, @RequestBody CreateTransactionRequest req, HttpSession session){
+        AccountEntity accountData;
+        try {
+            String token = (String) session.getAttribute(authSessionKey);
+            accountData = authService.validateToken(token);
+        } catch (Exception e) {
+            return ResponseEntity.status(UNAUTHORIZED).body("Unauthorized access");
+        }
+
+        ResponseInBoolean response = kasirService.editTransaction(transactionId, req, accountData);
+        if(response.isStatus()){
+            return ResponseEntity.ok(response.getMessage());
+        }
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(response.getMessage());
+    }
+}
